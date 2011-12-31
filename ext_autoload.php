69c32d8d77b8cf4a18054069e1049d5442b761f0
speedprogs_de <speedprogs_de@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-12-31 08:37:28 +0000

    version 2.0 beta
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sp_gallery/trunk@55925 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -6,11 +6,12 @@
 		'tx_spgallery_controller_gallerycontroller'                  => $extensionClassesPath . 'Controller/GalleryController.php',
 		'tx_spgallery_domain_model_gallery'                          => $extensionClassesPath . 'Domain/Model/Gallery.php',
 		'tx_spgallery_domain_model_image'                            => $extensionClassesPath . 'Domain/Model/Image.php',
+		'tx_spgallery_domain_repository_abstractrepository'          => $extensionClassesPath . 'Domain/Repository/AbstractRepository.php',
 		'tx_spgallery_domain_repository_galleryrepository'           => $extensionClassesPath . 'Domain/Repository/GalleryRepository.php',
 		'tx_spgallery_domain_repository_imagerepository'             => $extensionClassesPath . 'Domain/Repository/ImageRepository.php',
 		'tx_spgallery_hook_tca'                                      => $extensionClassesPath . 'Hook/Tca.php',
 		'tx_spgallery_hook_uxt3libtceformsinline'                    => $extensionClassesPath . 'Hook/UxT3libTceformsInline.php',
-		'tx_spgallery_object_objectbuilder '                         => $extensionClassesPath . 'Object/ObjectBuilder.php',
+		'tx_spgallery_object_objectbuilder'                          => $extensionClassesPath . 'Object/ObjectBuilder.php',
 		'tx_spgallery_persistence_registry'                          => $extensionClassesPath . 'Persistence/Registry.php',
 		'tx_spgallery_service_galleryimage'                          => $extensionClassesPath . 'Service/GalleryImage.php',
 		'tx_spgallery_task_directoryobserver'                        => $extensionClassesPath . 'Task/DirectoryObserver.php',
@@ -28,7 +29,7 @@
 		'tx_spgallery_viewhelpers_filesizeviewhelper'                => $extensionClassesPath . 'ViewHelpers/FileSizeViewHelper.php',
 		'tx_spgallery_viewhelpers_galleryviewhelper'                 => $extensionClassesPath . 'ViewHelpers/GalleryViewHelper.php',
 		'tx_spgallery_viewhelpers_locationviewhelper'                => $extensionClassesPath . 'ViewHelpers/LocationViewHelper.php',
-		'tx_spgallery_viewhelpers_teaserimagesviewhelper'            => $extensionClassesPath . 'ViewHelpers/TeaserImagesViewHelper.php',
 		'tx_spgallery_viewhelpers_rawviewhelper'                     => $extensionClassesPath . 'ViewHelpers/RawViewHelper.php',
+		'tx_spgallery_viewhelpers_teaserimagesviewhelper'            => $extensionClassesPath . 'ViewHelpers/TeaserImagesViewHelper.php',
 	);
 ?>
